1ce96c2d7b3d05abd41fef33557f41afa753ed3b
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-08-12 22:54:34 +0200

    Added nuget publish batch file

---
+++ publish.bat
@@ -0,0 +1,24 @@
+@echo off
+
+setlocal enableextensions enabledelayedexpansion
+
+for /D %%f in (*.*) do (
+    if exist %%f\bin rd /s /q %%f\bin
+    if exist %%f\obj rd /s /q %%f\obj
+)
+
+dotnet build -c Release
+
+for /R %%f in (*.nupkg) do call :fixup %%f
+goto end
+
+:fixup
+
+set fname1=%1
+set fname2=%fname1:symbols=%
+
+rem echo (%fname1%) == (%fname2%)
+if "%fname1%" == "%fname2%" nuget push "%1" -Source nuget.org
+goto :end
+
+:end
