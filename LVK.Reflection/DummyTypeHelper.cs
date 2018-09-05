3afc75337afc16dd8bf4bfa6cfa6e65f3d40af4c
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-09-05 10:17:08 +0200

    Remove unneeded file

--- LVK.Reflection/DummyTypeHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LVK.Reflection
-{
-    internal class DummyTypeHelper : ITypeHelper
-    {
-        public string TryGetNameOf(Type type, NameOfTypeOptions options = NameOfTypeOptions.Default) => type.FullName;
-    }
-}
