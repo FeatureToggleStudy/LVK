4ad9f2216e8a276fe70418ed7674ce056384a9de
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-09-06 13:18:56 +0200

    Add overloads for Log.LogXYZ to handle delegates

--- LVK.Logging/LoggerExtensions.cs
+++ LVK.Logging/LoggerExtensions.cs
@@ -14,28 +14,61 @@ namespace LVK.Logging
     [PublicAPI]
     public static class LoggerExtensions
     {
+        // ReSharper disable ExplicitCallerInfoArgument
         public static void LogTrace([NotNull] this ILogger logger, [CanBeNull] string message = null,
                                     [CallerFilePath] string callerFilePath = null,
                                     [CallerMemberName] string callerMemberName = null,
                                     [CallerLineNumber] int callerLineNumber = 0)
-            => Log(logger, LogLevel.Trace,
-                $"{(!string.IsNullOrWhiteSpace(message) ? message + " @ " : "")}{callerMemberName} @ {callerFilePath} #{callerLineNumber}");
+            => LogTrace(logger, () => message, callerFilePath, callerMemberName, callerLineNumber);
+        // ReSharper restore ExplicitCallerInfoArgument
+        
+        public static void LogTrace(
+            [NotNull] this ILogger logger, [NotNull] Func<string> getMessage,
+            [CallerFilePath] string callerFilePath = null, [CallerMemberName] string callerMemberName = null,
+            [CallerLineNumber] int callerLineNumber = 0)
+        {
+            if (getMessage == null)
+                throw new ArgumentNullException(nameof(getMessage));
+
+            Log(
+                logger, LogLevel.Trace, () =>
+                {
+                    string message = getMessage();
+                    return
+                        $"{(!string.IsNullOrWhiteSpace(message) ? message + " @ " : "")}{callerMemberName} @ {callerFilePath} #{callerLineNumber}";
+                });
+        }
 
         public static void LogDebug([NotNull] this ILogger logger, [NotNull] string message)
             => Log(logger, LogLevel.Debug, message);
 
+        public static void LogDebug([NotNull] this ILogger logger, [NotNull] Func<string> getMessage)
+            => Log(logger, LogLevel.Debug, getMessage);
+        
         public static void LogVerbose([NotNull] this ILogger logger, [NotNull] string message)
             => Log(logger, LogLevel.Verbose, message);
 
+        public static void LogVerbose([NotNull] this ILogger logger, [NotNull] Func<string> getMessage)
+            => Log(logger, LogLevel.Verbose, getMessage);
+        
         public static void LogInformation([NotNull] this ILogger logger, [NotNull] string message)
             => Log(logger, LogLevel.Information, message);
 
+        public static void LogInformation([NotNull] this ILogger logger, [NotNull] Func<string> getMessage)
+            => Log(logger, LogLevel.Information, getMessage);
+        
         public static void LogWarning([NotNull] this ILogger logger, [NotNull] string message)
             => Log(logger, LogLevel.Warning, message);
 
+        public static void LogWarning([NotNull] this ILogger logger, [NotNull] Func<string> getMessage)
+            => Log(logger, LogLevel.Warning, getMessage);
+        
         public static void LogError([NotNull] this ILogger logger, [NotNull] string message)
             => Log(logger, LogLevel.Error, message);
 
+        public static void LogError([NotNull] this ILogger logger, [NotNull] Func<string> getMessage)
+            => Log(logger, LogLevel.Error, getMessage);
+        
         public static void LogException([NotNull] this ILogger logger, [NotNull] Exception ex)
         {
             var sb = new StringBuilder();
@@ -65,19 +98,30 @@ namespace LVK.Logging
             logger.Log(logLevel, message);
         }
 
-        public static IDisposable LogScope([NotNull] this ILogger logger, LogLevel logLevel, [NotNull] string scopeName)
+        private static void Log([NotNull] ILogger logger, LogLevel logLevel, [NotNull] Func<string> getMessage)
         {
             if (logger == null)
                 throw new ArgumentNullException(nameof(logger));
 
-            if (scopeName == null)
-                throw new ArgumentNullException(nameof(scopeName));
+            logger.Log(logLevel, getMessage);
+        }
+
+        public static IDisposable LogScope([NotNull] this ILogger logger, LogLevel logLevel, [NotNull] string scopeName)
+            => LogScope(logger, logLevel, () => scopeName);
+
+        public static IDisposable LogScope([NotNull] this ILogger logger, LogLevel logLevel, [NotNull] Func<string> getScopeName)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            if (getScopeName == null)
+                throw new ArgumentNullException(nameof(getScopeName));
 
             Stopwatch sw = null;
 
             void openScope()
             {
-                logger.Log(logLevel, $"{scopeName}");
+                logger.Log(logLevel, getScopeName);
 
                 sw = Stopwatch.StartNew();
             }
@@ -87,10 +131,11 @@ namespace LVK.Logging
                 assume(sw != null);
                 sw.Stop();
 
-                logger.Log(logLevel, $"{scopeName}, finished in {sw.ElapsedMilliseconds} ms");
+                logger.Log(logLevel, () => $"{getScopeName()}, finished in {sw.ElapsedMilliseconds} ms");
             }
 
             return new ActionDisposable(openScope, closeScope);
         }
+
     }
 }
