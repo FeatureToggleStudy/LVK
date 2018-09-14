24884d932ed544a74b9bed3dac6e9450b89e1fbd
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-09-14 14:35:58 +0200

    Add per-machine and per-user appsettings.json file support

--- LVK.Configuration/StandardConfigurators/AppSettingsConfigurator.cs
+++ LVK.Configuration/StandardConfigurators/AppSettingsConfigurator.cs
@@ -1,11 +1,31 @@
-﻿namespace LVK.Configuration.StandardConfigurators
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+namespace LVK.Configuration.StandardConfigurators
 {
     internal class AppSettingsConfigurator : IConfigurationConfigurator
     {
+        [NotNull, ItemNotNull]
+        private IEnumerable<string[]> GetCombinations()
+        {
+            yield return new string[0];
+            yield return new[] { Environment.MachineName };
+            yield return new[] { Environment.UserName };
+            yield return new[] { Environment.MachineName, Environment.UserName };
+        }
+
         public void Configure(IConfigurationBuilder configurationBuilder)
         {
-            configurationBuilder.AddJsonFile("appsettings.json", isOptional: true);
-            configurationBuilder.AddJsonFile("appsettings.debug.json", isOptional: true);
+            foreach (string[] combination in GetCombinations())
+            {
+                string filename = string.Join(".", new[] { "appsettings" }.Concat(combination));
+                
+                configurationBuilder.AddJsonFile(filename + ".json", isOptional: true);
+                configurationBuilder.AddJsonFile(filename + ".debug.json", isOptional: true);
+            }
         }
     }
 }
