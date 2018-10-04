a4cf95d4cb6728f92dd0773ebbd51744b0f294de
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-10-04 11:21:39 +0200

    Add performance sandbox

---
+++ PerformanceSandbox/ApplicationEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using LVK.AppCore;
+
+namespace PerformanceSandbox
+{
+    public class ApplicationEntryPoint : IApplicationEntryPoint
+    {
+        public Task<int> Execute(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("executed");
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ PerformanceSandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+using LVK.AppCore.Console;
+
+namespace PerformanceSandbox
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            await ConsoleAppBootstrapper.RunAsync<ServicesBootstrapper>();
+        }
+    }
+}
