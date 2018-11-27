1f47b0493dd1a5b4e46829b858504d03382f060f
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-11-27 17:43:40 +0100

    Revert "Added web app example"
    This reverts commit bcb7e4dba107fe77829e2565f1f9ef5aaf61ead4.

--- ConsoleSandbox/Controllers/TestController.cs
+++ ConsoleSandbox/Controllers/TestController.cs
@@ -1,33 +1,14 @@
-using System;
-
-using JetBrains.Annotations;
-
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConsoleSandbox.Controllers
 {
-    public class TestModel
-    {
-        public TestModel(int counter) => Counter = counter;
-
-        public int Counter { get; }
-    }
-
     public class TestController : Controller
     {
-        [NotNull]
-        private readonly ICounterHolder _CounterHolder;
-
-        public TestController([NotNull] ICounterHolder counterHolder)
-        {
-            _CounterHolder = counterHolder ?? throw new ArgumentNullException(nameof(counterHolder));
-        }
-
         // GET
         [Route("/test")]
         public IActionResult Index()
         {
-            return View(new TestModel(_CounterHolder.CurrentValue));
+            return View();
         }
     }
 }
