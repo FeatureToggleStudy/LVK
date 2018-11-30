614a84bd6805cf722879a29b00a28587f769bb72
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-11-30 14:08:02 +0100

    Moved tests and non-test projects into separate folders in order to apply common build properties

---
+++ tests/LVK.DataStructures.Tests/PublicApiTests.cs
@@ -0,0 +1,11 @@
+using LVK.Tests.Framework;
+
+using NUnit.Framework;
+
+namespace LVK.DataStructures.Tests
+{
+    [TestFixture]
+    public class PublicApiTests : PublicApiTestsBase<ServicesBootstrapper>
+    {
+    }
+}
