cc034b865f017df1081d0fcab1c738d311d62348
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2019-05-10 13:46:03 +0200

    Add syncfusion component registration

---
+++ LVK.Syncfusion/RegisterSyncfusionComponentLibraries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using DryIoc;
+
+using JetBrains.Annotations;
+
+using LVK.Configuration;
+using LVK.DryIoc;
+
+namespace LVK.Syncfusion
+{
+    internal class RegisterSyncfusionComponentLibraries : IContainerFinalizer
+    {
+        [NotNull]
+        private readonly IConfigurationElementWithDefault<SyncfusionLicensingConfiguration> _Configuration;
+
+        public RegisterSyncfusionComponentLibraries([NotNull] IConfiguration configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            _Configuration = configuration["Licensing/Syncfusion"]
+               .Element<SyncfusionLicensingConfiguration>()
+               .WithDefault(() => new SyncfusionLicensingConfiguration());
+        }
+
+        public void Finalize(IContainer container)
+        {
+            var configuration = _Configuration.Value();
+
+            if (string.IsNullOrWhiteSpace(configuration.Key))
+                return;
+
+            global::Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(configuration.Key);
+        }
+    }
+}
