6d2bf15ee6bcf7f75f3634fc511e21d517a1fe8d
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2019-05-15 09:42:05 +0200

    Changed layers for json configuration files

--- ConsoleSandbox/FirstBackgroundService.cs
+++ ConsoleSandbox/FirstBackgroundService.cs
@@ -2,12 +2,23 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+using JetBrains.Annotations;
+
+using LVK.Configuration;
 using LVK.Core.Services;
 
 namespace ConsoleSandbox
 {
     internal class FirstBackgroundService : IBackgroundService
     {
+        [NotNull]
+        private readonly IConfigurationElementWithDefault<string> _Configuration;
+
+        public FirstBackgroundService([NotNull] IConfiguration configuration)
+        {
+            _Configuration = configuration.Element<string>("Name").WithDefault(() => "Unknown");
+        }
+
         public async Task Execute(CancellationToken cancellationToken)
         {
             Console.WriteLine("First background service starting");
@@ -15,7 +26,7 @@ namespace ConsoleSandbox
             {
                 while (!cancellationToken.IsCancellationRequested)
                 {
-                    Console.WriteLine("First");
+                    Console.WriteLine("First in " + _Configuration.Value());
                     await Task.Delay(5000, cancellationToken);
                 }
             }
